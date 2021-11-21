309db6ac0c6e5e45e9872bf28aaed6590ee43e4c
James Olszewski <lil.oski.one@gmail.com>
2021-11-21 18:59:34 -0500

    Add project files.

---
+++ Models/DepartmentLocation.cs
@@ -0,0 +1,16 @@
+using EmployeesCh12.Models;
+
+namespace EmployeesCh12.Models
+{
+    public class DepartmentLocation
+    {
+        // Composite Primary key and foreign key for Department
+        public int DepartmentID { get; set; }
+        // Composite Primary key and foreign key for Location
+        public int LocationID { get; set; }
+
+        // navigation properties
+        public Department Department { get; set; }
+        public Location Location { get; set; }
+    }
+}
